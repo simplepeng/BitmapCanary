cd15c52f5e683593bf886956c81ce24c45a6b4ab
chenpeng <383559698@qq.com>
2021-07-12 19:08:07 +0800

    更新：readme

---
+++ bitmap_canary_scanner/src/main/java/me/simple/bitmapcanary/Scanner.java
@@ -0,0 +1,9 @@
+package me.simple.bitmapcanary;
+
+public class Scanner {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("args = " + arg);
+        }
+    }
+}
