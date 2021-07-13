786a479034d2280bce7f35dcb4619bf0e22b1c3c
simple <383559698@qq.com>
2021-07-13 22:43:27 +0800

    更新

--- bitmap_canary_scanner/src/main/java/me/simple/bitmapcanary/Scanner.java
+++ bitmap_canary_scanner/src/main/java/me/simple/bitmapcanary/Scanner.java
@@ -1,9 +1,40 @@
 package me.simple.bitmapcanary;
 
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 public class Scanner {
+
     public static void main(String[] args) {
         for (String arg : args) {
             System.out.println("args = " + arg);
         }
+        File file = new File("");
+        System.out.println("args = " + file.getAbsolutePath());
+        String rootPath = file.getAbsolutePath();
+        File rootDir = new File(rootPath);
+        if (!rootDir.isDirectory()) {
+            log("rootDir is not Directory -- " + rootPath);
+            return;
+        }
+        traversals(rootDir);
+
+    }
+
+    private static void traversals(File file) {
+        File[] files = file.listFiles();
+        if (files == null) return;
+        for (File childFile : files) {
+            if (childFile.isDirectory()) {
+                traversals(childFile);
+            } else {
+                log("file - " + file.getAbsolutePath());
+            }
+        }
+    }
+
+    private static void log(String msg) {
+        System.out.println(msg);
     }
 }
