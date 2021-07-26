7c484394edd0b7de7fbb18df79edea5b291b5313
chenpeng <383559698@qq.com>
2021-07-26 16:44:02 +0800

    更新：抽出方法

--- bitmap_canary_scanner/src/main/java/me/simple/bitmapcanary/Scanner.java
+++ bitmap_canary_scanner/src/main/java/me/simple/bitmapcanary/Scanner.java
@@ -1,108 +1,28 @@
 package me.simple.bitmapcanary;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.imageio.ImageIO;
 
 public class Scanner {
 
-    private static List<File> analysisFiles = new ArrayList<>();
 
     public static void main(String[] args) {
-        for (String arg : args) {
-            System.out.println("args = " + arg);
-        }
-        File file = new File("");
-        System.out.println("project path = " + file.getAbsolutePath());
-        String rootPath = file.getAbsolutePath();
-        File rootDir = new File(rootPath);
-        if (!rootDir.isDirectory()) {
-            log("rootDir is not Directory -- " + rootPath);
-            return;
-        }
-        //
-        traversals(rootDir);
-        //
-        for (File file1 : analysisFiles) {
-            log("file1 - " + file1.getAbsolutePath());
-        }
-        //
-        startAnalysis();
-        //
-        analysisFiles.clear();
-    }
+        try {
+            String rootPath;
+            double maxValue ;
 
-    private static void traversals(File file) {
-        if (file.isFile()) {
-            addAnalysis(file);
-            return;
-        }
-        File[] files = file.listFiles();
-        if (files == null) {
-            return;
-        }
-        for (File child : files) {
-            if (child.isDirectory()) {
-                traversals(child);
+            if (args.length == 0) {
+                rootPath = new File("").getAbsolutePath();
+                maxValue = 1.00;
+            } else {
+                rootPath = args[0];
+                maxValue = Double.parseDouble(args[1]);
             }
-            if (child.isFile()) {
-                addAnalysis(child);
-            }
+
+            ScannerJob job = new ScannerJob();
+            job.run(rootPath, maxValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
         }
-    }
-
-    private static void addAnalysis(File file) {
-        String path = file.getAbsolutePath();
-        String parentName = file.getParent();
-        String name = file.getName();
-        if (parentName == null || parentName.length() < 1) return;
-        if ((parentName.contains("drawable") || parentName.contains("mipmap"))
-                && !path.contains("/build/generated")
-                && !name.endsWith("xml")) {
-            analysisFiles.add(file);
-        }
-    }
-
-    private static void startAnalysis() {
-        for (File file : analysisFiles) {
-            try {
-                log("path - " + file.getAbsolutePath());
-
-                if (file.getName().endsWith("webp")) {
-                    readWebp(file);
-                    continue;
-                }
-
-                BufferedImage image = ImageIO.read(file);
-                if (image == null) {
-                    log("image is null - " + file.getAbsolutePath());
-                    return;
-                }
-                log("width - " + image.getWidth());
-                log("height - " + image.getHeight());
-            } catch (Throwable e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private static void readWebp(File file) throws Exception {
-        FileInputStream fis = new FileInputStream(file);
-        byte[] bytes = new byte[30];
-        fis.read(bytes, 0, bytes.length);
-        int width = ((int) bytes[27] & 0xff) << 8 | ((int) bytes[26] & 0xff);
-        int height = ((int) bytes[29] & 0xff) << 8 | ((int) bytes[28] & 0xff);
-        log("width - " + width);
-        log("height - " + height);
-    }
-
-    private static void log(String msg) {
-        System.out.println(msg);
     }
 }
---
+++ bitmap_canary_scanner/src/main/java/me/simple/bitmapcanary/ScannerJob.java
@@ -0,0 +1,134 @@
+package me.simple.bitmapcanary;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.imageio.ImageIO;
+
+public class ScannerJob {
+
+    private final List<File> analyseFiles = new ArrayList<>();
+
+    private String mRootPath = "";
+    private double mMaxValue = 3.00;
+
+    private void println(String msg) {
+        System.out.println(msg);
+    }
+
+    public void run(String rootPath, double maxValue) {
+        this.mRootPath = rootPath;
+        this.mMaxValue = maxValue;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                File rootDir = new File(mRootPath);
+                println("rootPath -- " + rootDir.getAbsolutePath());
+                if (!rootDir.isDirectory()) {
+                    println("rootPath is not Directory");
+                    return;
+                }
+
+                //遍历
+                traversals(rootDir);
+                //开始分析
+                startAnalysis();
+                //清空集合
+                analyseFiles.clear();
+            }
+        }).start();
+    }
+
+    /**
+     * 递归遍历项目内所有目录
+     */
+    public void traversals(File file) {
+        if (file.isFile()) {
+            addAnalyse(file);
+            return;
+        }
+        File[] files = file.listFiles();
+        if (files == null) {
+            return;
+        }
+        for (File child : files) {
+            if (child.isDirectory()) {
+                traversals(child);
+            }
+            if (child.isFile()) {
+                addAnalyse(child);
+            }
+        }
+    }
+
+    /**
+     * 将符合规则的File添加进分析列表
+     */
+    public void addAnalyse(File file) {
+        String path = file.getAbsolutePath();
+        String parentName = file.getParent();
+        String name = file.getName();
+        if (parentName == null || parentName.length() < 1) return;
+        if ((parentName.contains("drawable") || parentName.contains("mipmap"))
+                && !path.contains("/build/generated")
+                && !name.endsWith("xml")) {
+            analyseFiles.add(file);
+        }
+    }
+
+    /**
+     * 开始分析
+     */
+    public void startAnalysis() {
+        for (File file : analyseFiles) {
+            try {
+                //webp
+                if (file.getName().endsWith("webp")) {
+                    readWebp(file);
+                    continue;
+                }
+                //png,jpg,gif
+                BufferedImage image = ImageIO.read(file);
+                if (image == null) {
+                    println("image is null - " + file.getAbsolutePath());
+                    return;
+                }
+                computeMemory(file, image.getWidth(), image.getHeight());
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * webP单独处理
+     */
+    public void readWebp(File file) throws Exception {
+        FileInputStream fis = new FileInputStream(file);
+        byte[] bytes = new byte[30];
+        fis.read(bytes, 0, bytes.length);
+        int width = ((int) bytes[27] & 0xff) << 8 | ((int) bytes[26] & 0xff);
+        int height = ((int) bytes[29] & 0xff) << 8 | ((int) bytes[28] & 0xff);
+        computeMemory(file, width, height);
+    }
+
+    /**
+     * 计算占用内存
+     */
+    public void computeMemory(File file, int width, int height) {
+        long memory = width * height * 4;
+        double maxByte = mMaxValue * 1024 * 1024;
+
+        if (memory >= maxByte) {
+            String m = String.format(Locale.getDefault(), "%.02fM", memory / 1024f / 1024f);
+            println("this bitmap is too large in memory  -- " + m);
+            println("path == " + file.getAbsolutePath());
+            println("---------------------------------------------------------------------------------------");
+        }
+    }
+}
